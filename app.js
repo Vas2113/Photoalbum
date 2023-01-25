bdf00379f1b4a7dd0d2c2a091e25cc4c9e41190d
Vasily <ololol2113@mail.ru>
2023-01-25 14:35:42 +0300

    config

--- app.js
+++ app.js
@@ -3,19 +3,17 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const ssr = require('./middleware/ssr');
-// const configApp = require('./config/serverConfig');
+const configApp = require('./config/serverConfig');
 
 const app = express();
 
 const PORT = 3000;
-// configApp(app);
+configApp(app);
 
 // const mainRoute = require('./routes/mainRoute');
 // const regRoute = require('./routes/regRoute');
 // const logoutRoute = require('./routes/logoutRoute');
 // const authRoutes = require('./routes/authRoutes');
-// const pageRoutes = require('./routes/page.routes');
-// const topicsRoutes = require('./routes/topicsRoutes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
