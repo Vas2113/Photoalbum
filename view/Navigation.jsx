25eeb54826bd7604ef433d7fc22508e6b17e4c58
Pavel <pavel.bezugliy@mail.ru>
2023-01-26 21:12:00 +0500

    NavBar

--- view/Navigation.jsx
+++ view/Navigation.jsx
@@ -1,34 +1,34 @@
-const React = require('react');
+const React = require("react");
 // const Log = require('./Log');
 // const Reg = require('./Reg');
 
 module.exports = function Navigation({ user }) {
-console.log(user,'useeeeeeeeeeeeeer')
+  console.log(user, "useeeeeeeeeeeeeer");
   return (
     <>
-      {' '}
-      {
-        !user ? (
-          <div className="navi">
-            <div className="reg">
-              <a href="/reg">
-                Регистрация
-              </a>
-            </div>
-            <div className="log">
-              <a href="/log">
-                Логинизация
-              </a>
-            </div>
+      {" "}
+      {!user ? (
+        <div className="navi">
+          <div className="reg">
+            <a href="/reg">Регистрация</a>
           </div>
-        ) : (
-          <div className="exit">
-            <a href="/logout">
-              Выйти
-            </a>
+          <div className="log">
+            <a href="/log">Логинизация</a>
           </div>
-        )
-      }
+        </div>
+      ) : (
+        <div className="exit">
+          <div className="myAlbum">
+            <a href="/main">Мои альбомы</a>
+          </div>
+          <div className="allAlbum">
+            <a href="/">Все доступные альбомы</a>
+          </div>
+          <div className="logout">
+            <a href="/logout">Выйти</a>
+          </div>
+        </div>
+      )}
     </>
   );
 };
